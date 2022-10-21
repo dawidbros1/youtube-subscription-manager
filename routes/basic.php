c4422b79df32b53eebde41ea17911817d16a3739
dawidbros1 <dawidbros1@wp.pl>
2022-10-21 16:26:03 +0200

    deleted old route

--- routes/basic.php
+++ routes/basic.php
@@ -8,24 +8,4 @@ $route = new Route($location);
 
 $route->homepage('home');
 
-$route->group("", "", [
-    'policy' => "/policy",
-    'contact' => "/contact",
-    'regulations' => "/regulations",
-]);
-
-$route->register("registration", "/register");
-$route->register("authorization", "/login");
-
-$route->group('user', "/user", [
-    'logout' => "/logout",
-    'profile' => "/profile",
-    'update' => "/profile/update",
-]);
-
-$route->group('passwordRecovery', "/password", [
-    'forgot' => "/forgot",
-    'reset' => "/reset",
-]);
-
 return $route;
