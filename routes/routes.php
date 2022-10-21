fb43eee501266623b0e00f35d3a7ac06e8707bcb
dawidbros1 <dawidbros1@wp.pl>
2022-10-21 16:25:56 +0200

    authorization route

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,9 @@ declare (strict_types = 1);
 
 $route = require_once "basic.php";
 
+$route->register('authorization', '/authorization', 'saveAccessToken');
+$route->register('authorization', '/logout', 'logout');
+
 // $route->group('test', "/test", [
 //     'show' => "/show/{id}",
 //     'show2' => "/show/{id}/{abc}/{category_id}",
