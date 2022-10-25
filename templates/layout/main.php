3151a7d77a7a384f3279dbbb4e532604a7de9fde
dawidbros1 <dawidbros1@wp.pl>
2022-10-25 17:51:23 +0200

    update in layout | render categories in sidebar and form to create category

--- templates/layout/main.php
+++ templates/layout/main.php
@@ -1,99 +1,109 @@
+<?php
+
+use Phantom\Component\Component;
+
+?>
+
 <html lang="pl">
 
 <head>
-    <title><?=$title?></title>
-    <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="<?=$location?>/public/css/style.css" rel="stylesheet">
+   <title><?=$title?></title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link href="<?=$location?>/public/css/style.css" rel="stylesheet">
+   <link href="<?=$location?>/public/css/sidebar.css" rel="stylesheet">
+   <link href="<?=$location?>/public/css/category.css" rel="stylesheet">
 
-    <?php if ($style): ?>
-        <link href="<?=$location?>/public/css/<?=$style?>.css" rel="stylesheet">
-    <?php endif;?>
+   <?php if ($style): ?>
+   <link href="<?=$location?>/public/css/<?=$style?>.css" rel="stylesheet">
+   <?php endif;?>
 
-    <script src='https://www.google.com/recaptcha/api.js'></script>
-    <script src="<?=$location?>/public/js/main.js"></script>
+   <script src='https://www.google.com/recaptcha/api.js'></script>
+   <script src="<?=$location?>/public/js/main.js"></script>
 </head>
 
 <body>
-    <div class="container-fluid">
+   <div class="container-fluid">
+      <div id="wrapper" class="toggled">
+         <div id="sidebar-wrapper" class="text-white">
+            <ul class="sidebar-nav">
+               <li class="sidebar-brand text-white">Nazwa aplikacji</li>
+               <li><a href="<?=$route->get('home')?>">Strona główna</a></li>
 
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <a class="navbar-brand ms-2 ms-md-3" href="<?=$route->get('home')?>">Strona główna</a>
+               <?php if ($user): ?>
+               <hr>
+               <div class="group">
+                  <div>Moje grupy</div>
 
-            <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-                <!-- Trigger -->
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="menu">
-
-            <!-- SET MARGIN WITH PHP -->
-            <?php $margin = "ms-2 ms-sm-0"?>
-                <!-- ===== LEFT POSITION MENU ===== -->
-                <!-- USER MENU -->
-                <ul class="navbar-nav me-auto">
-                    <?php if ($user): ?>
-                        <div class = "d-sm-flex <?=$margin?>">
-                            <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
-                            <li class="nav-item"> <a class="nav-link" href="#">Link 2</a></li>
-                            <li class="nav-item"> <a class="nav-link" href="#">Link 3</a></li>
+                  <?php foreach ($user->getCategories() ?? [] as $item): ?>
+                     <li>
+                        <div class="position-relative">
+                           <a href="<?=$route->get('category.show', [$item->get('id')])?>"><?=$item->get('name')?></a>
+                           <img
+                           class = "delete-form-handle icon-remove"
+                           src = "<?=$location?>/public/images/remove.png"
+                           data-name = <?=$item->get('name')?>
+                           data-action = <?=$route->get('category.delete', [$item->get('id')])?>
+                           >
                         </div>
+                     </li>
+                  <?php endforeach;?>
 
-                        <div class = "d-sm-none border-top"></div>
+                  <hr>
 
-                        <div class = "d-sm-none <?=$margin?>">
+                  <form id="create-category-form" action="<?=$route->get('category.create')?>" method="post">
+                     <div class = "d-flex flex-wrap">
+                        <input class="col-12" name="name" type = "text" placeholder="Nazwa grupu" value = "<?=$name?>">
+                        <button class="col-12 btn btn-primary fw-bold" type = "submit">Dodaj</button>
+                     </div>
+                     <?php Component::render('error', ['type' => "name", 'names' => ['between']])?>
+                  </form>
+               </div>
 
-                        </div>
-                    <?php endif;?>
-                </ul>
-                <!-- ===== RIGHT POSITION MENU ===== -->
-                <ul class="navbar-nav">
+               <hr>
+               <li><a href="<?=$route->get('authorization.logout')?>">Wyloguj</a></li>
+               <?php endif;?>
+            </ul>
+         </div>
+         <!-- /#sidebar-wrapper -->
 
-                <!-- GUEST MENU -->
-                <?php if (!$user): ?>
-                    <div class = "d-sm-none border-top"></div>
-                    <div class = "d-sm-flex <?=$margin?>">
-
-                    </div>
-                <?php endif;?>
-
-                <!-- USER MENU DROPDOWN ONLY ON SM+ -->
-                <?php if ($user): ?>
-                    <ul class="navbar-nav d-none d-sm-block">
-                        <!-- USERS -->
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <?=$user->getUsername()?>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
-                                <li><a class="dropdown-item" href="<?=$route->get('authorization.logout')?>">Wyloguj</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                <?php endif;?>
-                </ul>
+         <!-- Page Content -->
+         <div id="page-content-wrapper">
+            <div class="container-fluid">
+               <?php require_once 'templates/messages.php';?>
+               <?php require_once "templates/$page.php";?>
             </div>
-        </nav>
+            <!-- /#wrapper -->
 
-        <div class="content">
-            <?php require_once "templates/messages.php";?>
-            <?php require_once "templates/$page.php";?>
-        </div>
-    </div>
+            <!-- Menu Toggle Script -->
+            <script>
+            $("#menu-toggle").click(function(e) {
+               e.preventDefault();
+               $("#wrapper").toggleClass("toggled");
+            });
+            </script>
+         </div>
+      </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+      <div id = "category-delete-form-wrapper" class="d-none">
+         <div id="information">Czy jesteś pewny, że chcesz usunąć grupę <span id = "category-name" class="fw-bold"></span></div>
+         <form id = "category-delete-form" class="d-flex flex-wrap">
+            <button class="col-5 btn btn-success" id = "cancel" type = "button">NIE</button>
+            <button class="col-5 offset-2 btn btn-danger" type = "submit">TAK</button>
+         </form>
+      </div>
 
-   <!-- Remove the container if you want to extend the Footer to full width. -->
+   </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+   </script>
 
-
-   <footer class="bg-dark text-center text-white position-relative w-100 bottom-0">
-    <div class="text-center" style="background-color: rgba(0, 0, 0, 0.2);">Treść stopki</div>
-   </footer>
+   <script>
+      initToggleCategoryDeleteForm();
+   </script>
 </body>
 
 </html>
