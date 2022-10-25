96406523388bfefe5a9fe9050bebc148eac6135f
dawidbros1 <dawidbros1@wp.pl>
2022-10-25 17:52:16 +0200

    script to fill data in category-delete-form

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,25 @@ function initCloseAlertButtons() {
         })
     }
 }
+
+function initToggleCategoryDeleteForm() {
+    var wrapper = document.getElementById("category-delete-form-wrapper");
+    var cancel = document.getElementById('cancel');
+
+    cancel.addEventListener('click', () => {
+        wrapper.classList.add('d-none')
+    })
+
+    var category_name = document.getElementById("category-name");
+    var handles = document.getElementsByClassName('delete-form-handle');
+    var form = document.getElementById('category-delete-form');
+
+    for (let i = 0; i < handles.length; i++) {
+        let handle = handles[i];
+        handle.addEventListener('click', () => {
+            wrapper.classList.remove('d-none');
+            category_name.innerHTML = handle.dataset.name;
+            form.setAttribute('action', handle.dataset.action);
+        })
+    }
+}
