e8ac34e264ee4ef68900bc182009f7772186849a
dawidbros1 <dawidbros1@wp.pl>
2022-10-25 17:54:31 +0200

    added category routes and restored route to contact

--- routes/routes.php
+++ routes/routes.php
@@ -6,10 +6,14 @@ $route = require_once "basic.php";
 
 $route->register('authorization', '/authorization', 'saveAccessToken');
 $route->register('authorization', '/logout', 'logout');
+$route->register('', '/contact', 'contact');
 
-// $route->group('test', "/test", [
-//     'show' => "/show/{id}",
-//     'show2' => "/show/{id}/{abc}/{category_id}",
-// ]);
+$route->group('category', '/category', [
+    'list' => "/list",
+    'create' => "/create",
+    'edit' => "/edit/{id}",
+    'delete' => "/delete/{id}",
+    'show' => "/show/{id}",
+]);
 
 return $route;
