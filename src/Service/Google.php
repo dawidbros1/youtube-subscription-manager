ea080b31f9749fd8f09206d0896d9f8d60cf7dc0
dawidbros1 <dawidbros1@wp.pl>
2022-10-26 10:34:08 +0200

    changes in token autorefresh

--- src/Service/Google.php
+++ src/Service/Google.php
@@ -7,41 +7,28 @@ namespace App\Service;
 use App\Model\User;
 use Phantom\Helper\Session;
 use Phantom\Model\Route;
-use stdClass;
 
 class Google
 {
     private $client;
     private $url;
     private $route;
+    private $youtube;
+
     public function __construct(string $project_location, Route $route)
     {
         $this->route = $route;
-        $this->client = new \Google_client();
-        $this->client->setAuthConfig('client_secret.json');
-        $this->client->setAccessType('offline');
-
-        $this->client->setScopes([
-            \Google_Service_YouTube::YOUTUBE_READONLY,
-        ]);
-
-        $this->client->setApplicationName('API code samples');
         $this->url = $project_location;
-    }
-    public function getGoogleLoginUrl()
-    {
-        return filter_var($this->client->createAuthUrl(), FILTER_SANITIZE_URL);
+        $this->client = (new GoogleClient())->get();
+        $this->youtube = (new YoutubeService($this->client));
     }
 
     public function login()
     {
         if ($access_token = Session::get('access_token')) {
-            $this->client->setAccessToken($access_token);
-            $this->validateAccessToken();
+            $this->validateAccessToken($access_token);
 
-            $youtube = new \Google_Service_YouTube($this->client);
-            $channel = $youtube->channels->listChannels('snippet,id,contentDetails', ["mine" => true]);
-            return new User($channel[0]);
+            return new User($this->youtube->getMyChannel());
         }
 
         return null;
@@ -52,18 +39,9 @@ class Google
         $this->client->revokeToken();
     }
 
-    private function validateAccessToken()
+    public function getGoogleLoginUrl()
     {
-        if ($this->client->isAccessTokenExpired()) {
-            $class = new stdClass();
-            $class->login = false;
-
-            $this->client->setState(json_encode($class));
-            $authUrl = $this->client->createAuthUrl();
-
-            header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
-            exit();
-        }
+        return filter_var($this->client->createAuthUrl(), FILTER_SANITIZE_URL);
     }
 
     public function getClient()
@@ -73,6 +51,20 @@ class Google
 
     public function getYoutubeService()
     {
-        return new YoutubeService($this->client);
+        return $this->youtube;
+    }
+
+    private function validateAccessToken($access_token)
+    {
+        $this->client->setAccessToken($access_token);
+
+        if ($this->client->isAccessTokenExpired()) {
+            $refreshToken = $this->client->getRefreshToken();
+            $this->client->refreshToken($refreshToken);
+            $newAccessToken = $this->client->getAccessToken();
+            $newAccessToken['refresh_token'] = $refreshToken;
+            $this->client->setAccessToken($newAccessToken);
+            Session::set('access_tonen', $newAccessToken);
+        }
     }
 }
