5baceda9c7177675b4440659aa0fe7c528517943
dawidbros1 <dawidbros1@wp.pl>
2022-11-03 11:13:02 +0100

    introductory show template

--- templates/category/show.php
+++ templates/category/show.php
@@ -2,8 +2,14 @@
 
 declare (strict_types = 1);
 
-use Phantom\Component\Component;
-
 ?>
 
 JESTEM W SHOW <?=$category->get('name')?>
+
+<div class = "row">
+   <?php foreach ($videos as $video): ?>
+      <div class="col-12 col-sm-6 col-xl-4 col-xxl-3 my-1">
+          <?=$video->player->embedHtml?>
+      </div>
+   <?php endforeach;?>
+</div>
